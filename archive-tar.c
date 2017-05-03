d2287e77a2e3ac87a78dc7891d10f0be8937b80f
Ramsay Jones <ramsay@ramsayjones.plus.com>
2017-05-04 00:41:05 +0100

    archive-tar: fix a sparse 'constant too large' warning
    Commit dddbad728c ("timestamp_t: a new data type for timestamps",
    26-04-2017) introduced a new typedef 'timestamp_t', as a synonym for an
    unsigned long, which was used at the time to represent timestamps in
    git. A later commit 28f4aee3fb ("use uintmax_t for timestamps",
    26-04-2017) changed the typedef to use an 'uintmax_t' for the timestamp
    representation type.
    
    When building on a 32-bit Linux system, sparse complains that a constant
    (USTAR_MAX_MTIME) used to detect a 'far-future mtime' timestamp, is too
    large; 'warning: constant 077777777777UL is so big it is unsigned long
    long' on lines 335 and 338 of archive-tar.c. Note that both gcc and
    clang only issue a warning if this constant is used in a context that
    requires an 'unsigned long' (rather than an uintmax_t). (Since TIME_MAX
    is no longer equal to 0xFFFFFFFF, even on a 32-bit system, the macro
    USTAR_MAX_MTIME is set to 077777777777UL, which cannot be represented as
    an 'unsigned long' constant).
    
    In order to suppress the warning, change the definition of the macro
    constant to use an 'ULL' type suffix.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- archive-tar.c
+++ archive-tar.c
@@ -33,7 +33,7 @@ static int write_tar_filter_archive(const struct archiver *ar,
 #if TIME_MAX == 0xFFFFFFFF
 #define USTAR_MAX_MTIME TIME_MAX
 #else
-#define USTAR_MAX_MTIME 077777777777UL
+#define USTAR_MAX_MTIME 077777777777ULL
 #endif
 
 /* writes out the whole block, but only if it is full */
